02061f14c8b0906ef87c285cc4944f0c49198701
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2025-12-27 18:00:57 +0300

    Module 1 finished

--- src/app/features/events/event-list.ts
+++ src/app/features/events/event-list.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { EventCard } from './event-card';
+import { SearchBar } from './search-bar';
 
 @Component({
   selector: 'app-event-list',
-  imports: [EventCard],
+  imports: [EventCard, SearchBar],
   template: `
     <div class="mb-8">
       <h1 class="text-3xl font-bold text-gray-900 mb-4">Upcoming Events</h1>
       <!-- TODO Mod 1: Add SearchBar here -->
+      <app-search-bar [(query)]="searchQuery" />
+      <p class="text-gray-500 mt-2">Searching for: {{ searchQuery() }}</p>
     </div>
 
     <!-- TODO Mod 2: Wrap in @if (events.isLoading()) -->
@@ -15,12 +18,33 @@ import { EventCard } from './event-card';
       <!-- TODO Mod 2: Use @for to iterate over resource -->
 
       <!-- Static Placeholders for initial verify -->
-      <app-event-card />
-      <app-event-card />
-      <app-event-card />
+      <app-event-card
+        title="Angular Keynote"
+        image="/images/angular-keynote.png"
+        date="2025-12-10T09:00:00.000Z"
+        [initialLike]="true"
+        (delete)="onEventDeleted()"
+      />
+      <app-event-card
+        title="Signals Deep Dive"
+        image="/images/signals-deep-dive.png"
+        date="2025-12-30T09:00:00.000Z"
+        (delete)="onEventDeleted()"
+      />
+      <app-event-card
+        title="SSR at Scale"
+        image="/images/ssr-at-scale.png"
+        (delete)="onEventDeleted()"
+      />
     </div>
   `,
 })
 export class EventList {
   // TODO Mod 2: Inject Service and use resource()
+
+  searchQuery = signal('');
+
+  onEventDeleted(): void {
+    console.log('Delete Clicked!');
+  }
 }
